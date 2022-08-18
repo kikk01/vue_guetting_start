924e38e9d6fdf9317b477a9336d77c469b85ecc9
kikk01 <hugobar@live.fr>
2022-08-18 15:58:03 +0200

    ref

---
+++ src/components/Ref.vue
@@ -0,0 +1,26 @@
+<template>
+    <input ref="input">
+    <ul>
+      <li v-for="item in list" ref="items">{{ item }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+          1, 'test',2,3,4
+      ]
+    }
+  },
+  mounted() {
+    this.$refs.input.focus()
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/js/essentials/ref.js
@@ -0,0 +1,4 @@
+import { createApp} from "vue/dist/vue.esm-bundler";
+import Ref from '../../components/Ref.vue'
+
+createApp(Ref).mount('#ref')
